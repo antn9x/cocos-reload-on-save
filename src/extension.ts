a4dacfcba9152d7d2fcce6b25eb7454e02500c75
an <fe.nhatan@excitek.io>
2022-11-24 10:31:17 +0700

    add editor config

--- src/extension.ts
+++ src/extension.ts
@@ -6,28 +6,31 @@ const path = require("path");
 const fs = require("fs");
 
 function isCocosProject() {
-	if (!vscode.workspace.workspaceFolders) { return false; }
-	const rootPath = vscode.workspace.workspaceFolders[0];
-	if (!rootPath) { return false; }
-	const projectJsonPath = path.join(rootPath, "project.json");
-	if (fs.existsSync(projectJsonPath)) { return true; }
-	const packageJsonPath = path.join(rootPath, "package.json");
-	if (!fs.existsSync(packageJsonPath)) { return false; }
-	const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-	return packageJson.creator;
+  if (!vscode.workspace.workspaceFolders) {
+    // vscode.window.showInformationMessage('No folder or workspace opened');
+    return false;
+  }
+  const rootPath = vscode.workspace.workspaceFolders[0];
+  if (!rootPath) { return false; }
+  const projectJsonPath = path.join(rootPath, "project.json");
+  if (fs.existsSync(projectJsonPath)) { return true; }
+  const packageJsonPath = path.join(rootPath, "package.json");
+  if (!fs.existsSync(packageJsonPath)) { return false; }
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+  return packageJson.creator;
 }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const host = vscode.workspace.getConfiguration().get("host.host");
-	if (!isCocosProject()) { return; }
-	let timeout: NodeJS.Timeout;
-	vscode.workspace.onDidSaveTextDocument((e) => {
-		if (timeout && timeout.hasRef()) { clearTimeout(timeout); }
-		timeout = setTimeout(() => http.get(`${host}/update-db`).on("error", (err: Error) => console.log(err.message)), 200);
-	});
+  const host = vscode.workspace.getConfiguration().get("host.host");
+  if (!isCocosProject()) { return; }
+  let timeout: NodeJS.Timeout;
+  vscode.workspace.onDidSaveTextDocument((e) => {
+    if (timeout && timeout.hasRef()) { clearTimeout(timeout); }
+    timeout = setTimeout(() => http.get(`${host}/update-db`).on("error", (err: Error) => console.log(err.message)), 200);
+  });
 }
 
 // This method is called when your extension is deactivated
